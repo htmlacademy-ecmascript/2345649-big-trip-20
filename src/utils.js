2d5900a62611d6fd593e1819568627ca001ccd85
EYE <EYE@EYEz>
2023-05-31 12:07:27 +0300

    Добавит утилиты работы с датой

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,20 @@
+import dayjs from 'dayjs';
+import dur from 'dayjs/plugin/duration';
+dayjs.extend(dur);
+
+const DateFormat = {
+  SHORT_DATE: 'MMM DD',
+  LONG_DATE: 'YYYY-MM-DD',
+  TIME: 'hh:mm',
+  DATETIME: 'YYYY-MM-DDThh:mm',
+  DHM_DURATION: 'DD[D] HH[H] mm[M]',
+  HM_DURATION: 'HH[H] mm[M]',
+  M_DURATION: 'mm[M]',
+};
+
+const MSEC_IN_DAY = 1000 * 60 * 60 * 24;
+const MSEC_IN_HOUR = 1000 * 60 * 60;
+
 function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
@@ -18,4 +35,46 @@ function getRandomInteger(a, b) {
   return Math.floor(result);
 }
 
-export { getRandomArrayElement, createIdGenerator, getRandomInteger };
+function shortDate(date) {
+  return date ? dayjs(date).format(DateFormat.SHORT_DATE) : '';
+}
+
+function longDate(date) {
+  return date ? dayjs(date).format(DateFormat.LONG_DATE) : '';
+}
+
+function timeOfDay(date) {
+  return date ? dayjs(date).format(DateFormat.TIME) : '';
+}
+
+function dateTime(date) {
+  return date ? dayjs(date).format(DateFormat.DATETIME) : '';
+}
+
+function duration(start, end) {
+  const dateDiff = dayjs(end).diff(dayjs(start));
+  let formattedDuration = '';
+
+  if (dateDiff >= MSEC_IN_DAY) {
+    formattedDuration = dayjs
+      .duration(dateDiff)
+      .format(DateFormat.DHM_DURATION);
+  } else if (dateDiff >= MSEC_IN_HOUR) {
+    formattedDuration = dayjs.duration(dateDiff).format(DateFormat.HM_DURATION);
+  } else {
+    formattedDuration = dayjs.duration(dateDiff).format(DateFormat.M_DURATION);
+  }
+
+  return formattedDuration;
+}
+
+export {
+  getRandomArrayElement,
+  createIdGenerator,
+  getRandomInteger,
+  shortDate,
+  longDate,
+  timeOfDay,
+  dateTime,
+  duration,
+};
